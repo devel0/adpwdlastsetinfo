0593b20d4c2dbbe3a818423d9ec83e9db1da678f
Lorenzo Delana <oss.devel@searchathing.com>
2018-09-17 18:13:18 +0200

    initial import

---
+++ adpwdlastsetinfo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace adpwdlastsetinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 2)
+            {
+                System.Console.WriteLine($"Synopsis: {AppDomain.CurrentDomain.FriendlyName} <pwdLastSet> [expireDays]");
+                System.Console.WriteLine($"Description: returns human readable datetime of given pwdLastSet long number and days from last set");
+                System.Console.WriteLine($"             if expireDays given exitcode==2 if password expired");
+                System.Environment.Exit(1);
+            }
+            long age = long.Parse(args[0]);
+            Console.WriteLine($"pwdLastSet: {DateTime.FromFileTimeUtc(age)}");
+            var daysFromLastSet = (DateTime.Now - DateTime.FromFileTimeUtc(age)).TotalDays;
+            Console.WriteLine($"daysFromLastSet: {daysFromLastSet}");
+            if (args.Length == 2)
+            {
+                int expireDays = int.Parse(args[1]);
+                Environment.Exit((daysFromLastSet <= expireDays) ? 0 : 2);
+            }            
+        }
+    }
+}
